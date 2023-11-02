50ec9567fe068a6c0bd8fa87479fef762d8ef8d6
Ramesh Kumar M K <mkrk.ramesh006@gmail.com>
2023-11-02 16:12:31 +0530

    created songlist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SongListComponent } from './song-list/song-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongListComponent
   ],
   imports: [
     BrowserModule
