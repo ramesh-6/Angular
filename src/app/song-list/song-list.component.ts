38d9a8db8c69012e9e4c85c1f1537727fa6f8fa5
Ramesh Kumar M K <mkrk.ramesh006@gmail.com>
2023-11-06 15:00:31 +0530

    Updated song-list.html and component ts file update list, getcount

--- src/app/song-list/song-list.component.ts
+++ src/app/song-list/song-list.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./song-list.component.css']
 })
 export class SongListComponent {
+  newSong:string='';
+  songList: string[] = ['song1','song2','song3','Song4'];
+  listCount:number = this.songList.length;
+  isDisplay:boolean = false;
 
+  updateList(){
+    this.songList.push(this.newSong);
+    this.isDisplay=false;
+    console.log('Updating new song :', this.newSong);
+  }
+
+  getcount(){
+    this.isDisplay=true;
+    this.listCount=this.songList.length;
+    console.log('listCount is now:', this.listCount);
+  }
 }
